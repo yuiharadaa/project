49d52959dac83a0ed8856dbd96a754a9332e3036
yuiharadaa <0429.yuiyui@gmail.com>
2025-05-26 00:47:26 +0900

    トップページとテンプレートの表示処理を追加

--- diary/views.py
+++ diary/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import View
+
+class IndexView(View):
+    def get(self, request):
+        message="ようこそ日記アプリへ！"
+        return render(request, 'diary/index.html', {"message": message})
+    
+index = IndexView.as_view()
 
 # Create your views here.
