430f585e239b8aba90857d025718346dfe2b844f
yuiharadaa <0429.yuiyui@gmail.com>
2025-06-04 17:41:15 +0900

    一覧の表示

--- diary/views.py
+++ diary/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views import View
 from .forms import PageForm
+from .models import Page
 
 class IndexView(View):
     def get(self, request):
@@ -18,8 +19,15 @@ class PageCreateView(View):
             form.save()
             return redirect("diary:index")
         return render(request, 'diary/page_form.html', {'form': form})
+    
+class PageListView(View):
+    def get(self, request):
+        page_list = Page.objects.all()
+        return render(request, 'diary/page_list.html', {'page_list': page_list})
 
 index = IndexView.as_view()
 page_create = PageCreateView.as_view()
+page_list = PageListView.as_view()
+
 
 # Create your views here.
