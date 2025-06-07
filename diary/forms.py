4a97e614365af57313f30f3c410efb092433ef22
yuiharadaa <0429.yuiyui@gmail.com>
2025-06-07 16:13:29 +0900

    日付をカレンダー表示に変更

--- diary/forms.py
+++ diary/forms.py
@@ -1,7 +1,11 @@
 from django.forms import ModelForm
 from .models import Page
+from django.forms.widgets import DateInput
 
 class PageForm(ModelForm):
     class Meta:
         model = Page
         fields = ['title', 'body', 'page_date']
+        widgets = {
+            'page_date': DateInput(attrs={'type': 'date'})
+        }
